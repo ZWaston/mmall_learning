0235f6f02de710b7ad77242941cbbabb96341bf0
ZWaston <code1995@126.com>
2018-06-14 17:38:25 +0800

    用户模块增加，通过测试

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,22 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    int checkUsername(String username);
+
+    int checkEmail(String email);
+
+    //登录之后，需要将用户信息返回给前端，用mybatis传入多个参数时，需要加注解,注解内定义的就是写sql语句的对应的名字
+    User selectLogin(@Param("username") String username, @Param("password")String password);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(@Param("username")String username,@Param("question")String question,@Param("answer")String answer);
+
+    int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew")String passwordNew);
+
+    int checkPassword(@Param("password")String password,@Param("userID")Integer userID);
+
+    int checkEmailByUserId(@Param("email")String email,@Param("userID")Integer userId);
+
 }
